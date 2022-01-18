b2212b8a48546c6e78f80d194695b3afb2afa21d
Md. Aminul Islam <89296239+aminilislam@users.noreply.github.com>
2022-01-18 22:37:56 +0600

    add this by me

--- js/index.js
+++ js/index.js
@@ -18,23 +18,45 @@ $(document).ready(function() {
 
   });
 
-});
 
-var typed = new Typed('.element', {
-  strings: ['You-tuber', 'Web-Developer','You-tuber', 'Free-Lancer', 'Web-designer'],
-  typeSpeed: 60,
-  backSpeed: 50,
-  backDelay: 2700,
-  loop: true
+  var typed = new Typed('.element', {
+    strings: ['You-tuber', 'Web-Developer','You-tuber', 'Free-Lancer', 'Web-designer'],
+    typeSpeed: 60,
+    backSpeed: 50,
+    backDelay: 2700,
+    loop: true
+    
   
-
-});
-var typed = new Typed('.elementt', {
-  strings: ['You-tuber', 'Web-Developer','You-tuber', 'Free-Lancer', 'Web-designer'],
-  typeSpeed: 60,
-  backSpeed: 50,
-  backDelay: 2700,
-  loop: true
+  });
+  var typed = new Typed('.elementt', {
+    strings: ['You-tuber', 'Web-Developer','You-tuber', 'Free-Lancer', 'Web-designer'],
+    typeSpeed: 60,
+    backSpeed: 50,
+    backDelay: 2700,
+    loop: true
+    
   
+  });
+
+  var waypoints = $('#handler-first').waypoint(function(direction) {
+    notify(this.element.id + ' hit 25% from top of window') 
+  })
+
+  $('.image').waypoint(function(direction) {
+    $('.image').addClass('animate__animated animate__rollIn')
+  },{
+    offset: '200px'
+  })
+  $('.about-text').waypoint(function(direction) {
+    $('.about-text').addClass('animate__animated animate__fadeInRight')
+  },{
+    offset: '200px'
+  })
+
+  $('a').smoothScroll();
 
 });
+
+
+
+
